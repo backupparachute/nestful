27687adea855e0696042746e4ccade0103eeed49
kyle <github@kylemiller.com>
2019-10-09 17:55:54 -0500

    Revert "Mirror Faradys URI encoding - ampersands were not being encoded properly"
    This reverts commit 860af4f852be600c3d074561d9598d33aad24470.

--- lib/nestful/helpers.rb
+++ lib/nestful/helpers.rb
@@ -78,12 +78,8 @@ module Nestful
       URI.encode_www_form_component(s.to_s)
     end
 
-    ESCAPE_RE = /[^a-zA-Z0-9 .~_-]/
-    
     def uri_escape(s)
-      s.to_s.gsub(ESCAPE_RE) {|match|
-        '%' + match.unpack('H2' * match.bytesize).join('%').upcase
-      }.tr(' ', '+')
+      URI.escape(s.to_s.scrub)
     end
 
     if defined?(::Encoding)
